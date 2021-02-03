16e231398df862ae693a753dac84f7cdcfcc1238
Richard Moseley <rwmoseley1219@gmail.com>
2021-02-03 02:17:51 -0700

    added a list of all votes for candidates, and added them to a list. counted those votes based on candidate

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -29,9 +29,9 @@ with open(readFile) as csvfile:
             canList.append(i[2])
         if i[2] not in canVote["Candidate"]:
             canVote["Candidate"].append(i[2])
-        # allCan_data.append(i[2])
-        # for j in canVote["Candidate"]:
-        #     canVote["Votes"] = allCan_data.count(j)
+        allCan_data.append(i[2])
+    for j in canVote["Candidate"]:
+        canVote["Votes"].append(allCan_data.count(j))
 
 
 print(canVote)
