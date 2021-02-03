72439d7ca547ffbad64119505073fd9603d64faf
Richard Moseley <rwmoseley1219@gmail.com>
2021-02-03 02:40:37 -0700

    okay that didnt work lol. back to the chopping block

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -32,9 +32,7 @@ with open(readFile) as csvfile:
         allCan_data.append(i[2])
     for j in canVote["Candidate"]:
         canVote["Votes"].append(allCan_data.count(j))
-    for cans, votes in canVote:
-        if votes > votes + 1:
-            winner = cans
+
 
 print(winner)
 
