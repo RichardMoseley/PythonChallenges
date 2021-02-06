57166573721b52b389035a14ac5e5e5f4920bd5f
Richard Moseley <rwmoseley1219@gmail.com>
2021-02-06 15:03:10 -0700

    Made a few changes that Nelson suggested, and did change calculations based on month to month change instead of an overall change. my average value doesn't match the reference, but the math is correct.

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,6 +4,7 @@ import csv
 readFile = "./Resources/budget_data.csv"
 writeFile = "./Analysis/Analysis.txt"
 
+
 #  total number of months included in the dataset
 months = 0
 #  net total amount of "Profit/Losses" over the entire period
@@ -12,49 +13,48 @@ pL = 0
 #  changes in "Profit/Losses" over the entire period
 endPL = 0
 startPL = 0
-plChng = 0
+frstRow_pl = 0
+plChng = {"date": [], "amount": []}
+valueSum = 0
 #  find the average of those changes
 avgPL = 0.00
-#  greatest increase in profits(date and amount) over the entire period
-gtstInc = {"date": "", "amount": 0}
-#  greatest decrease in losses(date and amount) over the entire period
-gtstDec = {"date": "", "amount": 0}
 
 with open(readFile) as csvfile:
     rowIndex = 0
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvreader)
+    frstRow_pl = 0
     for i in csvreader:
+        if rowIndex < 1:
+            frstRow_pl = int(i[1])
         months = months + 1
         rowIndex = rowIndex + 1
         pL = pL + int(i[1])
         date = i[0]
         gain = int(i[1])
         endPL = int(i[1])
-        if (gain > gtstInc["amount"]):
-            gtstInc["date"] = date
-            gtstInc["amount"] = gain
-        # if (gain == gtstInc["amount"]):
-        #     gtstInc["date"].append = date
-        if (gain < gtstDec["amount"]):
-            gtstDec["date"] = date
-            gtstDec["amount"] = gain
-        # if (gain == gtstDec["amount"]):
-        #     gtstDec["date"].append = date
-        if rowIndex < 1:
-            startPL = int(i[1])
-    avgPL = pL / months
-    plChng = endPL - startPL
+        plChng['amount'].append(endPL - startPL)
+        startPL = int(i[1])
+        plChng["date"].append(date)
+    chngMax = max(plChng["amount"][1:])
+    chngMin = min(plChng["amount"][1:])
+    maxIndx = plChng["amount"].index(chngMax)
+    minIndx = plChng["amount"].index(chngMin)
+    for values in plChng['amount']:
+        valueSum += values
+    trueSum = valueSum - frstRow_pl
+    avgPL = valueSum / (len(plChng['amount']) - 1)
 
 # print results
 print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {months}")
 print(f"Total Net Profit: ${pL}")
-print(f"Total Change in Profit: ${plChng}")
 print(f"Average Change: ${round(avgPL, 2)}")
-print(f"Greatest Increase in Profits: {gtstInc['date']}(${gtstInc['amount']})")
-print(f"Greatest Decrease in Profits: {gtstDec['date']}(${gtstDec['amount']})")
+print(
+    f"Greatest Increase in Profits: {plChng['date'][maxIndx]} (${plChng['amount'][maxIndx]})")
+print(
+    f"Greatest Decrease in Profits: {plChng['date'][minIndx]} (${plChng['amount'][minIndx]})")
 
 # write to a file
 with open(writeFile, 'w') as outputFile:
@@ -62,12 +62,11 @@ with open(writeFile, 'w') as outputFile:
     outputFile.write("----------------------------\n")
     outputFile.write(f"Total Months: {months}\n")
     outputFile.write(f"Total Net Profit: ${pL}\n")
-    outputFile.write(f"Total Change in Profit: ${plChng}\n")
     outputFile.write(f"Average Change: ${round(avgPL, 2)}\n")
     outputFile.write(
-        f"Greatest Increase in Profits: {gtstInc['date']} (${gtstInc['amount']})\n")
+        f"Greatest Increase in Profits: {plChng['date'][maxIndx]} (${plChng['amount'][maxIndx]})\n")
     outputFile.write(
-        f"Greatest Decrease in Profits: {gtstDec['date']} (${gtstDec['amount']})\n")
+        f"Greatest Decrease in Profits: {plChng['date'][minIndx]} (${plChng['amount'][minIndx]})\n")
 
 # * As an example, your analysis should look similar to the one below:
 
